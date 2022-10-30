2ddc8dc5cd7dadf0f9ffb3ab5c938f50cd9d3086
Babasyuk <artem.babasyuk@mail.ru>
2022-10-30 09:48:42 +0300

    Work this logic

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var greetingLabel: UILabel!
+    
+    @IBOutlet var greetingButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        greetingLabel.isHidden.toggle()
+        greetingButton.layer.cornerRadius = 10
+        
     }
-
-
+    
+    @IBAction func greetingButtonDidTapped() {
+        greetingLabel.isHidden.toggle()
+        greetingButton.setTitle(
+            greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting",
+            for: .normal
+        )
+//        if greetingLabel.isHidden {
+//            greetingButton.setTitle("Show Greeting", for: .normal)
+//        } else {
+//            greetingButton.setTitle("Hide Greeting", for: .normal)
+//        }
+        
+        
+        
+    }
+    
 }
-
